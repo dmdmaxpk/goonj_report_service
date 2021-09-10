c7cc91a757e07f074d4c90746bd1c24a4714fec3
taha <taha@dmdmax.com>
2021-09-10 13:05:48 +0500

    fix

--- repos/BillingHistoryRepo.js
+++ repos/BillingHistoryRepo.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const BillingHistory = mongoose.model('BillingHistory');
 const Subscription = mongoose.model('Subscription');
+const moment = require('moment');
 
 class BillingHistoryRepository {
     constructor(){
