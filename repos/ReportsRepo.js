0574d13a641f9fca89cf29cf77743581c58be7c1
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-09-18 19:44:34 +0500

    double charge report

--- repos/ReportsRepo.js
+++ repos/ReportsRepo.js
@@ -2881,29 +2881,29 @@ computeDoubleChargeUsers = async () => {
 
                     let hour = Number(obj.hour);
 
-                    if(hour >= 1 && hour < 5){
-                        finalObj.cycle_1 = obj.price
+                    if(hour >= 0 && hour < 5){
+                        finalObj.cycle_1 = finalObj.hasOwnProperty('cycle_1') ? String(finalObj.cycle_1) + ", " + String(obj.price) : String(obj.price);
                     }
                     if(hour >= 5 && hour < 8){
-                        finalObj.cycle_5 = obj.price;
+                        finalObj.cycle_5 = finalObj.hasOwnProperty('cycle_5') ? String(finalObj.cycle_5) + ", " + String(obj.price) : String(obj.price);
                     }
                     if(hour >= 8 && hour < 11){
-                        finalObj.cycle_8 = obj.price;
+                        finalObj.cycle_8 = finalObj.hasOwnProperty('cycle_8') ? String(finalObj.cycle_8) + ", " + String(obj.price) : String(obj.price);
                     }
                     if(hour >= 11 && hour < 14){
-                        finalObj.cycle_11 = obj.price;
+                        finalObj.cycle_11 = finalObj.hasOwnProperty('cycle_11') ? String(finalObj.cycle_11) + ", " + String(obj.price) : String(obj.price);
                     }
                     if(hour >= 14 && hour < 17){
-                        finalObj.cycle_14 = obj.price;
+                        finalObj.cycle_14 = finalObj.hasOwnProperty('cycle_14') ? String(finalObj.cycle_14) + ", " + String(obj.price) : String(obj.price);
                     }
                     if(hour >= 17 && hour < 20){
-                        finalObj.cycle_17 = obj.price;
+                        finalObj.cycle_17 = finalObj.hasOwnProperty('cycle_17') ? String(finalObj.cycle_17) + ", " + String(obj.price) : String(obj.price);
                     }
                     if(hour >= 20 && hour < 22){
-                        finalObj.cycle_20 = obj.price;
+                        finalObj.cycle_20 = finalObj.hasOwnProperty('cycle_20') ? String(finalObj.cycle_20) + ", " + String(obj.price) : String(obj.price);
                     }
                     if(hour >= 22){
-                        finalObj.cycle_22 = obj.price;
+                        finalObj.cycle_22 = finalObj.hasOwnProperty('cycle_22') ? String(finalObj.cycle_22) + ", " + String(obj.price) : String(obj.price);
                     }
                 }
             }
